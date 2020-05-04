2ae8b8c8be95a25dbfa49457244cd2a28601d8e9
abhishekpawar96 <abhishekpawar96@gmail.com>
2020-05-05 01:01:47 +0530

    add builder pattern

--- src/main/java/com/patterns/Main.java
+++ src/main/java/com/patterns/Main.java
@@ -1,12 +1,27 @@
 package com.patterns;
 
+import com.patterns.builder.LunchOrder;
+import com.patterns.builder.LunchOrder.Builder;
 import com.patterns.singleton.DBSingleton;
 
 public class Main {
 
     public static void main(String[] args) {
+        // Singleton Demo
         DBSingleton instance = DBSingleton.getInstance();
         System.out.println(instance);
+
+        // Builder Demo
+        Builder builder = new Builder();
+        builder.bread("Wheat")
+                .condiments("Lettuce")
+                .dressing("Mayo")
+                .meat("Ham");
+        LunchOrder lunchOrder = builder.build();
+        System.out.println(lunchOrder.getBread());
+        System.out.println(lunchOrder.getCondiments());
+        System.out.println(lunchOrder.getDressing());
+        System.out.println(lunchOrder.getMeat());
 
     }
 
