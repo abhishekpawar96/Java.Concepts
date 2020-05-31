ec2ce40219db3d82f35e796f7ead55d14b9601a0
abhishekpawar96 <abhishekpawar96@gmail.com>
2020-05-31 22:01:22 +0530

    Add package

---
+++ src/main/java/com/patterns/composite/MenuComponent.java
@@ -0,0 +1,28 @@
+package com.patterns.composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class MenuComponent {
+
+    String name;
+    String url;
+    List<MenuComponent> menuComponents = new ArrayList<>();
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public abstract String toString();
+
+    String print(MenuComponent menuComponent) {
+        StringBuilder builder = new StringBuilder(name);
+        builder.append(": ").append(url).append("\n");
+        return builder.toString();
+    }
+
+}
