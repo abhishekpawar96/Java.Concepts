df13da9d93326f949f04b9ec19dc5a7ecf2f268f
abhishekpawar96 <abhishekpawar96@gmail.com>
2020-06-01 00:26:36 +0530

    Add package

---
+++ src/main/java/com/patterns/decorator/Sandwich.java
@@ -0,0 +1,5 @@
+package com.patterns.decorator;
+
+public interface Sandwich {
+   String make();
+}
---
+++ src/main/java/com/patterns/decorator/SimpleSandwich.java
@@ -0,0 +1,10 @@
+package com.patterns.decorator;
+
+public class SimpleSandwich implements Sandwich {
+
+    @Override
+    public String make() {
+        return "Simple Sandwich";
+    }
+
+}
